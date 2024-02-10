edfa4dfd0823e53efd2ad1d59e79fae4dc34bdfc
Alon4ic <o.redjko@gmail.com>
2024-02-10 13:00:13 +0200

    End of course

--- 05-frontend-and-backend/backend/app.mjs
+++ 05-frontend-and-backend/backend/app.mjs
@@ -1,20 +1,24 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
-
 app.use(morgan('tiny'));
-
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-
+// enable all CORS requests
+app.use(cors())
 
 app.use((req, res) => {
+    const personData = {
+        name: 'Alon4ik',
+        isStudent: true,
+    };
     console.log(req.body);
-    return res.send('Thies is Express server');
+    return res.json(personData);
 });
 
 app.listen(5000, () => {
